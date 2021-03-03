281972b7b640a52196ca0954d94fa69cc20034a1
Seo-young Silvia Kim <sskim.research@gmail.com>
2021-03-03 18:21:52 -0500

    proj init

---
+++ init.R
@@ -0,0 +1,19 @@
+renv::init()
+install.packages("devtools")
+install.packages("remotes")
+install.packages("colorspace")
+library(remotes)
+install_github(
+  "sysilviakim/Kmisc", INSTALL_opts = c("--no-multiarch"), dependencies = TRUE
+)
+Kmisc::proj_skeleton()
+
+# Install typically used libraries
+install.packages("plyr")
+install.packages("tidyverse")
+install.packages("lubridate")
+install.packages("here")
+install.packages("assertthat")
+install.packages("styler")
+
+renv::snapshot()
