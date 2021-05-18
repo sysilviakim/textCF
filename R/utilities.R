0e6ca19b8415b75d15adec5d382aeb2fb56bde46
Seo-young Silvia Kim <sskim.research@gmail.com>
2021-05-18 15:38:34 -0400

    init common source utilities

---
+++ R/utilities.R
@@ -0,0 +1,19 @@
+# Tidyverse ====================================================================
+library(plyr)
+library(tidyverse)
+library(lubridate)
+
+# Text analysis packages =======================================================
+library(quanteda)
+library(quanteda.textplots)
+library(quanteda.textstats)
+
+# Other packages ===============================================================
+library(here)
+library(janitor)
+library(assertthat)
+
+# Functions ====================================================================
+
+# Other options ================================================================
+
