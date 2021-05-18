18ba4721d8adf270cf72ecbc182dcfaacc00306d
Seo-young Silvia Kim <sskim.research@gmail.com>
2021-05-18 16:16:36 -0400

    delete comments + section code

--- R/winred_image_initial_code.R
+++ R/winred_image_initial_code.R
@@ -1,4 +1,6 @@
 source(here::here("R", "utilities.R"))
+
+# Import =======================================================================
 winred_dat <- loadRData(
   here("data", "raw", "2022", "winred", "winred_text_scraped.Rda")
 ) %>%
@@ -11,48 +13,7 @@ summary(winred_dat)
 ## The images we need are linked in the following columns: logo and bgimg
 ## NOT og_image and twitter_image
 
-##### Explanation of Procedure, and of Trial-and-Error #####
-
-# To download the images, I employed the download.file() function from the utils
-# package.
-# download.file() takes a series of arguments, three of which were necessary for
-# my purposes here. Those three are:
-## 'url' - the URL of the file to be downloaded;
-## 'destfile' - the place where the File is to be saved, and its name; and
-## 'method' 
-# download.file() became the key tool within the functions I wrote, which 
-# employed for-loops to download each individual URL in a given column
-# Ultimately, the functions employed took the following general form:
-###image_download_type <- function(dat, image, name) {
-###  for (i in 1:nrow(dat)) {
-###    filepath[i] <- paste("pathway to directory where the image is 
-###                          to be saved",name[i],"_type.jpg", sep="")
-###    download.file(url = image[i],
-###                  destfile = filepath[i],
-###                  method = "curl")
-###  }
-###}
-# In this context, 'dat' is the dataset where the URL is contained, 'image' is
-# the column of the dataset with the URL, and 'name' is a vector containing a
-# name that can be given to the image file.
-# This function takes a dataset with columns containing a URL and a name vector.
-# This version, and the versions in the code below, hardcodes a computer-
-# specific file path, which ought to be amended.
-# The filepath[i] line is necessary to create a line-specific file name for all
-# images. If the destfile is a constant, every image will be saved with the same
-# name, tanking any efforts to compile in bulk. 
-# It should be the case that, with a bit of editing to the filepath[i] line's 
-# paste call, it can be structured such that the file can be sent to a given
-# location with a call along the lines of
-## destfile =  here("winred_images", filepath[i])
-# In this circumstance, with here() replacing the need for the sprawling, 
-# hardcoded file path, 'filename[i]' would be the better name for such a column.
-# I shall use this approach while coding on the Democratic side of things.
-# Putting image[i] in quotes in the 'url' line (url = "image[i]",) will
-# tank the code - it is essentially saying that the URL is, verbatim, image[i],
-# which of course it is not.
-
-##### Logo Downloads #####
+# Logo downloads ===============================================================
 
 logo_dat <- winred_dat %>%
   select(name_clean, logo) %>%
@@ -61,9 +22,7 @@ logo_dat <- winred_dat %>%
 assert_that(!any(duplicated(logo_dat$name_clean)))
 image_download_logo_updated(logo_dat, image = "logo", name = "name_clean")
 
-# These are all the logos downloaded.
-
-##### bgimg Downloads #####
+# bgimg oownloads ==============================================================
 
 bg_dat <- as.data.frame(cbind(winred_dat$name_clean, winred_dat$bgimg))
 names(bg_dat) <- c("name_clean", "bgimg")
