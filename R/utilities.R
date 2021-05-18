d398d12b811d157e66f4e6526e9bc656eec683b3
Brian Brew <bbrew@live.unc.edu>
2021-05-18 17:28:52 -0400

    added image_download_bgimg_updated function to the utilities

--- R/utilities.R
+++ R/utilities.R
@@ -37,4 +37,24 @@ image_download_logo_updated <- function(dat, image, name) {
   }
 }
 
+image_download_bgimg_updated <- function(dat, image, name) {
+  path <- here("data/raw/2022/winred/bgimg")
+  if (!dir.exists(path)) dir.create(path)
+  
+  for (i in 1:nrow(dat)) {
+    download.file(
+      url = dat[[image]][i],
+      destfile = file.path(
+        path,
+        paste0(
+          gsub("_{1,}$", "", gsub("_{2,}", "_", trimws(dat[[name]][i]))),
+          "_bgimg_", format(Sys.Date(), "%Y%m%d"), ".jpg"
+        )
+      ),
+      method = "curl"
+    )
+    Sys.sleep(3)
+  }
+}
+
 # Other options ================================================================
