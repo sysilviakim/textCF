527055dc637096c7eba81b785e54a8cfa0ddd102
Seo-young Silvia Kim <sskim.research@gmail.com>
2021-07-27 00:25:02 -0400

    change name for winred filtering file

---
+++ R/01_anedot_federal_filter.R
@@ -0,0 +1,5 @@
+source(here::here("R", "utilities.R"))
+
+# Load Anedot scrape results ===================================================
+load(here("data/raw/2022/rightus/anedot_text_scraped.Rda"))
+
