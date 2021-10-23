bad381c064d8ca0315881df3e94cf365ae1313d6
Seo-young Silvia Kim <sskim.research@gmail.com>
2021-10-23 15:39:01 -0400

    NGP VAN preliminary

--- R/06_federal_wayback_minor_platforms.R
+++ R/06_federal_wayback_minor_platforms.R
@@ -7,22 +7,57 @@ load(here("data", "tidy", "winred_congress.Rda"))
 load(here("data", "tidy", "anedot_congress.Rda"))
 
 # What's missing? ==============================================================
-x <- "senate"
-test <- Reduce(bind_rows, list(actblue[[x]], winred[[x]], anedot[[x]]))
-##  14 rows ( 9.7%)
-View(
-  anti_join(wayback_std(cong[[x]]), test, by = "url") %>%
-    ## Can't go back and see PayPal
-    filter(!grepl("paypal", url))
+cong_raw <- cong
+cong <- cong_raw %>%
+  imap(
+    ~ anti_join(
+      wayback_std(.x),
+      Reduce(bind_rows, list(actblue[[.y]], winred[[.y]], anedot[[.y]])) %>%
+        ## Can't go back and see PayPal
+        filter(!grepl("paypal", url)),
+      by = "url"
+    )
 )
 
-## 130 rows (15.9%)
-x <- "house"
-test <- Reduce(bind_rows, list(actblue[[x]], winred[[x]], anedot[[x]]))
-View(
-  anti_join(wayback_std(cong[[x]]), test, by = "url") %>%
-    filter(!grepl("paypal", url))
-)
+## 16 and 137
+cong %>% map_dbl(nrow)
+
+# NGP VAN ======================================================================
+cong %>%
+  imap(
+    ~ {
+      ## Three types: act.myngp, actions.ngpvan, secure.ngpvan
+      df <- .x %>% filter(grepl("ngp", url))
+      out <- vector(mode = "list", length = nrow(df))
+      fp <- here("data", "raw", "wayback", .y, "html")
+      
+      for (x in seq(nrow(df))) {
+        tryCatch(
+          {
+            ## Because this relies on RSelenium, need to access
+            ## WayBack machine directly
+            out[[x]] <- ngp_select_text(df$link[x]) %>%
+              select(-date)
+            out[[x]] <- bind_cols(
+              out[[x]] %>% rename(link = url), df[x, c("url")]
+            ) %>%
+              select(url, -link, everything())
+          },
+          error = function(e) {
+            message(e)
+          }
+        )
+        
+        if ((x %% 50 == 0) | x == nrow(df)) {
+          save(
+            out,
+            file = here("data/raw", paste0("ngpvan_text_", .y, "_list.Rda"))
+          )
+        }
+      }
+    }
+  )
+
 
 # Duplicate scrapes per URL ====================================================
 
