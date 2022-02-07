24bd382878a433541fc6ea6b085f23d233e6a5c4
Seo-young Silvia Kim <sskim.research@gmail.com>
2022-02-06 19:44:22 -0500

    equiv change for senate data

--- R/03_fb_library_import_senate.R
+++ R/03_fb_library_import_senate.R
@@ -14,6 +14,18 @@ senate <- read_csv(
   ## 75 ---> 72
   filter(!is.na(id))
 
+## Those who were not up for reelection
+senate <- bind_rows(
+  senate,
+  read_csv(
+    here("data", "raw", "fb", "fb_senate_not_running_2020.csv"),
+    col_types = cols(.default = "c")
+  ) %>%
+    select(candidate, state = state_po, id = fb_ad_library_id) %>%
+    filter(!is.na(id))
+) %>%
+  dedup()
+
 ## Radlibrary will only accept 10 page IDs at a time; run by each ID
 assert_that(!any(duplicated(senate$candidate)))
 assert_that(!any(grepl("E", senate$id)))
@@ -25,12 +37,17 @@ senate_list <- senate %>%
   `names<-`({.} %>% map(~ .x$candidate[1]) %>% unlist())
 
 # Run FB Graph API =============================================================
-fname <- here("data", "raw", "fb", "fb-senate-raw-ads-2020.Rda")
-if (file.exists(fname)) {
-  load(fname)
+fname1 <- here("data", "raw", "fb", "fb-senate-raw-ad-data-2020.Rda")
+fname2 <- here("data", "raw", "fb", "fb-senate-raw-demo-data-2020.Rda")
+fname3 <- here("data", "raw", "fb", "fb-senate-raw-region-data-2020.Rda")
+if (file.exists(fname1)) {
+  load(fname1)
+  load(fname2)
+  load(fname3)
 } else {
-  fb_senate <- vector("list", length(senate_list))
-  names(fb_senate) <- names(senate_list)
+  ad_senate <- vector("list", length(senate_list))
+  names(ad_senate) <- names(senate_list)
+  region_senate <- demo_senate <- ad_senate
 }
 
 for (i in seq(length(senate_list))) {
@@ -40,20 +57,38 @@ for (i in seq(length(senate_list))) {
   Sys.sleep(3)
   
   if (!grepl("e", tolower(idx))) {
-    if (is.null(fb_senate[[cand]])) {
-      fb_senate[[cand]] <- fb_short(id = idx, token = token)
-      assert_that(!is.null(fb_senate[[cand]]))
-      print(head(fb_senate[[cand]]$tbl))
-      Sys.sleep(5)
+    if (is.null(ad_senate[[cand]])) {
+      ## Three chunks of preset groups of data
+      ad_senate[[cand]] <-
+        fb_short(id = idx, token = token, fields = "ad_data")
+      assert_that(!is.null(ad_senate[[cand]]))
+      Sys.sleep(3)
+      
+      demo_senate[[cand]] <- 
+        fb_short(id = idx, token = token, fields = "demographic_data")
+      assert_that(!is.null(demo_senate[[cand]]))
+      Sys.sleep(3)
+      
+      region_senate[[cand]] <- 
+        fb_short(id = idx, token = token, fields = "region_data")
+      assert_that(!is.null(region_senate[[cand]]))
+      Sys.sleep(3)
+      
+      print(head(ad_senate[[cand]]$tbl))
     }
   }
-  save(fb_senate, file = fname)
+  save(ad_senate, file = fname1)
+  save(demo_senate, file = fname2)
+  save(region_senate, file = fname3)
   message(paste0("Finished for ", cand, ", ", senate_list[[i]]$state, "."))
   message(paste0("Row ", i, " out of ", length(senate_list), "."))
 }
 
+# Accidentally left out candidates? ============================================
+ad_senate %>% map("tbl") %>% map_lgl(is.null) %>% which()
+
 # Which candidates go over 5,000? ==============================================
-vec <- fb_senate %>%
+vec <- ad_senate %>%
   map("tbl") %>%
   map(nrow) %>% 
   ## map_lgl gives type compliance errors:
@@ -79,27 +114,37 @@ date_breaks <- c(
 for (i in idx_retry) {
   idx <- senate_list[[i]]$id
   cand <- senate_list[[i]]$candidate
-  fb_senate[[cand]] <- list() 
+  ad_senate[[cand]] <- list() 
   ## Run by two months intervals then combine the rows
   for (x in seq(length(date_breaks) - 1)) {
-    fb_senate[[cand]][[x]] <- fb_short(
-      id = idx, token = token, 
+    ad_senate[[cand]][[x]] <- fb_short(
+      id = idx, token = token, fields = "ad_data", 
       min_date = date_breaks[x],
       max_date = date_breaks[x + 1]
     )
-    if (!is.null(nrow(fb_senate[[cand]][[x]]$tbl))) {
-      assert_that(nrow(fb_senate[[cand]][[x]]$tbl) < 5000)
+    demo_senate[[cand]][[x]] <- fb_short(
+      id = idx, token = token, fields = "demographic_data", 
+      min_date = date_breaks[x],
+      max_date = date_breaks[x + 1]
+    )
+    region_senate[[cand]][[x]] <- fb_short(
+      id = idx, token = token, fields = "region_data", 
+      min_date = date_breaks[x],
+      max_date = date_breaks[x + 1]
+    )
+    if (!is.null(nrow(ad_senate[[cand]][[x]]$tbl))) {
+      assert_that(nrow(ad_senate[[cand]][[x]]$tbl) < 5000)
       message(paste0("1mo interval starting from ", date_breaks[x], " done."))
-      message(paste0("Number of rows was ", nrow(fb_senate[[cand]][[x]]$tbl)))
+      message(paste0("Number of rows was ", nrow(ad_senate[[cand]][[x]]$tbl)))
     }
     Sys.sleep(5)
   }
-  fb_senate[[cand]]$tbl <- fb_senate[[cand]] %>% map("tbl") %>% bind_rows()
-  assert_that(!is.null(fb_senate[[cand]]))
-  assert_that(nrow(fb_senate[[cand]]$tbl) > 5000)
-  save(fb_senate, file = fname)
+  ad_senate[[cand]]$tbl <- ad_senate[[cand]] %>% map("tbl") %>% bind_rows()
+  assert_that(!is.null(ad_senate[[cand]]))
+  assert_that(nrow(ad_senate[[cand]]$tbl) > 5000)
+  save(ad_senate, file = fname)
   message(paste0("Finished for ", cand, ", ", senate_list[[i]]$state, "."))
 }
 
 # Final check ==================================================================
-fb_senate %>% map("tbl") %>% map(nrow) %>% unlist() %>% length()
+ad_senate %>% map("tbl") %>% map(nrow) %>% unlist() %>% length()
