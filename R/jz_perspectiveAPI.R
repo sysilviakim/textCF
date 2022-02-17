62cd9c449c3eeb57fb6e39ab5575061018d7090a
Jan Z <jz981@nyu.edu>
2022-02-17 10:44:49 +0100

    pespective API estimation for US House and Senate candidate ads

---
+++ R/jz_perspectiveAPI.R
@@ -0,0 +1,86 @@
+library(peRspective)
+library(tidyverse)
+
+source("R/utilities.R")
+
+# usethis::edit_r_environ()
+# Sys.setenv(perspective_api_key = "AIzaSyBiSxBAIcVz_IvXPqF19TDkjFkjqrW8gLA")
+# load(here("output", "fb_quanteda.Rda"))
+
+load(here("data", "tidy", "fb_unique.Rda"))
+
+
+batchHouse1 <- fbh %>%
+  filter(persp_id %in% c(1:20000)) %>%
+  prsp_stream(text = ad_creative_body,
+              text_id = persp_id,
+              languages = "en",
+              score_model = c("IDENTITY_ATTACK", "TOXICITY", "OBSCENE")
+  )
+
+batchHouse2 <- fbh %>%
+  filter(persp_id %in% c(20000:max(fbh$persp_id))) %>%
+  prsp_stream(text = ad_creative_body,
+              text_id = persp_id,
+              languages = "en",
+              score_model = c("IDENTITY_ATTACK", "TOXICITY", "OBSCENE")
+  )
+
+batch_combineHOUSE <- bind_rows(
+    batchHouse1,
+    batchHouse2
+    )
+
+fbHouse_Merged <- left_join(fbh,
+                            batch_combineHOUSE %>% 
+                            dplyr::rename(persp_id = text_id))
+
+fbHouse_Merged %>%
+    saveRDS(here("data", "tidy", "fb_perspective_House.RDS")) 
+
+
+
+# ------------------------------
+# Estimate Senate data
+# ------------------------------
+fbs <- fb_unique$senate %>%
+  mutate(persp_id = row_number())
+
+
+batchSen1 <- fbs %>%
+  filter(persp_id %in% c(1:10000)) %>%
+  prsp_stream(text = ad_creative_body,
+              text_id = persp_id,
+              languages = "en",
+              score_model = c("IDENTITY_ATTACK", "TOXICITY", "OBSCENE")
+              )
+
+batchSen2 <- fbs %>%
+  filter(persp_id %in% c(10001:20000)) %>%
+  prsp_stream(text = ad_creative_body,
+              text_id = persp_id,
+              languages = "en",
+              score_model = c("IDENTITY_ATTACK", "TOXICITY", "OBSCENE")
+              )
+
+batchSen3 <- fbs %>%
+  filter(persp_id %in% c(20001:max(fbs$persp_id))) %>%
+  prsp_stream(text = ad_creative_body,
+              text_id = persp_id,
+              languages = "en",
+              score_model = c("IDENTITY_ATTACK", "TOXICITY", "OBSCENE")
+              )
+
+batch_combineSENATE <-
+  bind_rows(
+    batchSen1,
+    batchSen2,
+    batchSen3
+  )
+
+fbSenate_Merged <- left_join(fbs,
+                   batch_combineSENATE %>% 
+                    dplyr::rename(persp_id = text_id))
+
+fbSenate_Merged %>%
+  saveRDS(here("data", "tidy", "fb_perspective_Senate.RDS")) 
