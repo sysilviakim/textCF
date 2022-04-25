98fde1e9f67f93f5e3da17147b0884021f6edd8a
Seo-young Silvia Kim <sskim.research@gmail.com>
2022-04-25 15:37:27 -0400

    smaller batch size + fix file name/bind_rows

--- R/06_perspective_FB_estimate.R
+++ R/06_perspective_FB_estimate.R
@@ -2,37 +2,55 @@ library(peRspective)
 source(here::here("R", "utilities.R"))
 load(here("data", "tidy", "fb_unique.Rda"))
 vec <- c(senate = "senate", house = "house")
+batch_size <- 1e2
 
 # Estimate House/Senate data with perspective API ==============================
 for (chamber in vec) {
   df <- fb_unique[[chamber]] %>% mutate(persp_id = row_number())
-  
+
   ## Batch-loop
-  batch_list <- vector("list", (nrow(df) %/% 1e2) + 1)
-  for (i in seq(nrow(df) %/% 1e2 + 1)) {
+  batch_list <- vector("list", (nrow(df) %/% batch_size) + 1)
+  for (i in seq(nrow(df) %/% batch_size + 1)) {
     batch_list[[i]] <- df %>%
-      filter(persp_id %in% seq(1e2 * (i - 1) + 1, max(1e2 * i, nrow(df)))) %>%
+      filter(
+        persp_id %in%
+          seq(batch_size * (i - 1) + 1, min(batch_size * i, nrow(df)))
+      ) %>%
       prsp_stream(
         text = ad_creative_body,
         text_id = persp_id,
         languages = "en",
         score_model = c("TOXICITY", "OBSCENE")
       )
+    
     print(paste0(i, "-th batch finished for ", chamber, "."))
-    save(batch_list, file = here("out", paste0("batch_", chamber, "_list.Rda")))
+    Sys.sleep(30) ## prevent HTTP 429 resource exhausted error
+    
+    save(
+      batch_list,
+      file = here(
+        "output",
+        paste0(
+          "batch_", chamber, "_list_", format(Sys.Date(), "%Y%m%d"), ".Rda"
+        )
+      )
+    )
   }
   print("All batches finished!! ==============================================")
-  
-  fb_merged <- left_join(
-    df, 
-    batch_list %>% bind_rows() %>% rename(persp_id = text_id)
-  )
-  
+
+  temp <- batch_list %>%
+    bind_rows() %>%
+    rename(persp_id = text_id)
+  assert_that(nrow(df) == nrow(temp))
+  fb_merged <- left_join(df, temp) %>% clean_names()
+
   fb_merged %>%
     saveRDS(
       here(
-        "data", "tidy", 
-        paste0("fb_perspective_", chamber, "_", ymd(Sys.Date()), ".RDS")
+        "data", "tidy",
+        paste0(
+          "fb_perspective_", chamber, "_", format(Sys.Date(), "%Y%m%d"), ".RDS"
+        )
       )
     )
 }
@@ -40,7 +58,13 @@ for (chamber in vec) {
 df <- vec %>%
   map(
     ~ base::readRDS(
-      max(list.files(here("data", "tidy"), pattern = "fb_perspective_", .x))
+      max(
+        list.files(
+          here("data", "tidy"),
+          pattern = paste0("fb_perspective_", .x),
+          full.names = TRUE
+        )
+      )
     )
   ) %>%
   bind_rows(., .id = "chamber")
