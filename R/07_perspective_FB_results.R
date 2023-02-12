8e129abea909693d8a72399eb4069f47ab1d060f
Seo-young Silvia Kim <sskim.research@gmail.com>
2023-02-12 12:43:38 -0500

    delete deprecated code

--- R/07_perspective_FB_results.R
+++ R/07_perspective_FB_results.R
@@ -3,6 +3,8 @@ source(here::here("R", "utilities.R"))
 # Load data ====================================================================
 load(here("data", "tidy", "fb_matched.Rda"))
 load(here("data", "tidy", "fb_unique.Rda"))
+
+## Wrangle data
 fb_matched <- fb_matched %>%
   map(
     ~ .x %>%
@@ -45,6 +47,7 @@ fb_matched <- fb_matched %>%
   )
 save(fb_matched, file = here("data", "tidy", "fb_matched_wrangled.Rda"))
 
+## Load the latest perspective results
 load(
   max(
     list.files(
@@ -57,6 +60,7 @@ load(
 df <- df %>% select(candidate, ad_creative_body, chamber, toxicity, obscene)
 
 # Merge data ===================================================================
+## This is the main dataset that will be used
 df_unique <- bind_rows(
   left_join(fb_unique$senate, df %>% filter(chamber == "senate")) %>%
     mutate(chamber = "Senate"),
@@ -72,6 +76,8 @@ df_unique <- bind_rows(
 assert_that(!any(is.na(df_unique$toxicity)))
 save(df_unique, file = here("data", "tidy", "merged_unique.Rda"))
 
+## Just in case we want to perform weighting with duplicate-included
+## dataset, keep all data
 df_all <- bind_rows(
   left_join(fb_matched$senate, df %>% filter(chamber == "senate")) %>%
     mutate(chamber = "Senate"),
@@ -159,264 +165,3 @@ pdf(
 )
 print(p2)
 dev.off()
-
-# Subtypes within financial ads ================================================
-# p3 <- fb_perspective_plot(
-#   summ_donor_unique,
-#   full = TRUE,
-#   xvar = "Toxic", se = "se_toxic",
-#   xlab = paste0(
-#     "Average Probability that Ad Text Would Be\n",
-#     "Perceived as Rude/Disrespectful/Toxic"
-#   )
-# )
-# pdf(here("fig", "fb_toxic_financial_subtypes.pdf"), width = 6, height = 2.8)
-# print(p3)
-# dev.off()
-
-# By party =====================================================================
-candidate_Tox <- df %>%
-  filter(chamber == "House") %>%
-  group_by(candidate, party) %>%
-  dplyr::summarise(
-    `Avg. prob. toxic` = mean(toxicity),
-    N = n()
-  )
-
-toxDEMcand <- candidate_Tox %>%
-  filter(party == "Democrat") %>%
-  filter(N >= 20) %>%
-  ggplot(aes(x = `Avg. prob. toxic`)) +
-  geom_histogram() +
-  labs(
-    y = "Candidates",
-    x = "Average usage of toxic language",
-    title = "Democratic Candidates"
-  ) +
-  geom_vline(
-    aes(xintercept = mean(`Avg. prob. toxic`)),
-    col = "blue", size = 1.5
-  ) +
-  geom_text(
-    aes(
-      x = mean(`Avg. prob. toxic`) + .03,
-      y = 25,
-      label = paste0("Mean:", mean(`Avg. prob. toxic`) %>% round(3))
-    )
-  ) +
-  theme_minimal() +
-  theme(plot.title = element_text(hjust = 0.5))
-
-toxREPcand <- candidate_Tox %>%
-  filter(party == "Republican") %>%
-  filter(N >= 20) %>%
-  ggplot(aes(x = `Avg. prob. toxic`)) +
-  geom_histogram() +
-  labs(
-    y = "Candidates",
-    x = "Average usage of toxic language",
-    title = "Republican Candidates"
-  ) +
-  geom_vline(
-    aes(xintercept = mean(`Avg. prob. toxic`)),
-    col = "red", size = 1.5
-  ) +
-  geom_text(
-    aes(
-      x = mean(`Avg. prob. toxic`) + .05,
-      y = 25,
-      label = paste0("Mean:", mean(`Avg. prob. toxic`) %>% round(3))
-    )
-  ) +
-  theme_minimal() +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  ylim(c(0, 30))
-
-ggarrange(toxDEMcand, toxREPcand) %>%
-  annotate_figure(
-    top = text_grob(
-      "Distribution of candidate-level average ad toxicity (US House)",
-      face = "bold", size = 14
-    )
-  )
-ggsave(here("fig/fig2_house_candidate_averages.pdf"), width = 8, height = 5)
-
-candidate_Tox %>%
-  filter(N >= 20) %>%
-  filter(party %in% c("Democrat", "Republican")) %>%
-  ggplot(
-    aes(
-      y = party,
-      x = `Avg. prob. toxic`,
-      color = party,
-      label = candidate
-    )
-  ) +
-  geom_boxplot() +
-  geom_jitter(height = .25) +
-  scale_color_brewer(palette = "Set1", direction = -1) +
-  labs(y = "")
-ggsave(here("fig/fig2B_house_candidate_averages.pdf"), width = 8, height = 4)
-
-# Keeping a type if N>100 for any party group:
-keepPostType <- fb_house %>%
-  dplyr::count(party, type) %>%
-  filter(n >= 100, type != "") %>%
-  mutate(post = glue("{type} posted by {party}")) %>%
-  pull(post)
-
-fb_house %>%
-  mutate(post = glue("{type} posted by {party}")) %>%
-  filter(post %in% keepPostType) %>%
-  ggplot(aes(y = post, x = toxicity, fill = post)) +
-  ggridges::geom_density_ridges() +
-  xlim(c(0, .3)) +
-  theme(legend.position = "none") +
-  labs(
-    y = "",
-    x = "Prob. text would be perceived as rude/disrespectful/toxic",
-    title = "US House",
-    subtitle = "Type shown if at least 100 posts of a given type exists"
-  )
-ggsave(here("fig/ridges_by_type.pdf"), width = 8, height = 6)
-
-# Substantial interpretations ==================================================
-percentage_calc("Donor", "House") ## 31.2
-percentage_calc("Donor", "Senate") ## 15.7
-
-percentage_calc("Voter", "House") ## 33.6
-percentage_calc("Voter", "Senate") ## 28.8
-
-percentage_calc(c = "House", p = "Democrat") ## 35.3
-percentage_calc(c = "House", p = "Republican") ## 32.9
-
-percentage_calc(c = "Senate", p = "Democrat") ## 29.4
-percentage_calc(c = "Senate", p = "Republican") ## 16.2
-
-# Example of Toxic Ads =========================================================
-## Dem
-fb_house %>%
-  filter(toxicity >= .75) %>%
-  filter(party == "Democrat") %>%
-  pull(ad_creative_body)
-
-## Rep
-fb_house %>%
-  filter(toxicity >= .75) %>%
-  filter(party == "Republican") %>%
-  pull(ad_creative_body)
-
-fb_house %>%
-  ggplot(aes(x = toxicity)) +
-  geom_histogram() +
-  geom_vline(aes(xintercept = mean(toxicity)), col = "red", size = 1.5)
-
-tox_dem <- fb_house %>%
-  filter(party == "Democrat") %>%
-  ggplot(aes(x = toxicity)) +
-  geom_histogram() +
-  labs(
-    x = "", y = "Number of ads",
-    title = "Democrats"
-  ) +
-  geom_vline(aes(xintercept = mean(toxicity)), col = "blue", size = 1.5) +
-  geom_text(
-    aes(
-      x = mean(toxicity) + .2,
-      y = 1000,
-      label = paste0("Mean:", mean(toxicity) %>% round(2))
-    )
-  )
-
-tox_rep <- fb_house %>%
-  filter(party == "Republican") %>%
-  ggplot(aes(x = toxicity)) +
-  geom_histogram() +
-  labs(
-    x = "", y = "Number of ads",
-    title = "Republicans"
-  ) +
-  geom_vline(aes(xintercept = mean(toxicity)), col = "red", size = 1.5) +
-  geom_text(
-    aes(
-      x = mean(toxicity) + .15,
-      y = 1000,
-      label = paste0("Mean:", mean(toxicity) %>% round(2))
-    )
-  )
-
-ggarrange(tox_rep, tox_dem) %>%
-  annotate_figure(
-    top = text_grob("Distribution of toxicity probabilities in Facebook ads",
-      face = "bold", size = 14
-    ),
-    bottom = text_grob("Prob(Ad text is toxic)")
-  )
-ggsave(here("fig/SI_ad_level_toxicity.pdf"), width = 8, height = 5)
-
-# Initial analysis =============================================================
-fb_house %>%
-  group_by(party) %>%
-  dplyr::summarise(`Toxic` = mean(toxicity), `Number of ads` = n())
-
-fb_house %>%
-  group_by(type) %>%
-  dplyr::summarise(
-    `Toxic` = mean(toxicity),
-    `Number of ads` = n()
-  ) %>%
-  dplyr::arrange(-Toxic) %>%
-  filter(type != "")
-
-fb_house %>%
-  group_by(party, type) %>%
-  dplyr::summarise(
-    `Toxic` = mean(toxicity), `Obscene` = mean(obscene), N = n()
-  ) %>%
-  filter(N >= 100, type != "") %>%
-  mutate(post = glue("{type} posted by {party}")) %>%
-  pivot_longer(cols = c(Toxic, `Obscene`)) %>%
-  ggplot(aes(y = post, x = value, color = name)) +
-  geom_point() +
-  facet_grid(~name)
-
-temp0notext <- candidate_Tox %>%
-  filter(N >= 10) %>%
-  filter(party %in% c("Democrat", "Republican")) %>%
-  ggplot(
-    aes(
-      y = party,
-      x = `Avg. prob. toxic`,
-      color = party,
-      label = candidate
-    )
-  ) +
-  geom_jitter() +
-  scale_color_brewer(palette = "Set1", direction = -1) +
-  labs(y = "")
-
-temp1 <- candidate_Tox %>%
-  filter(N >= 10) %>%
-  filter(party %in% c("Democrat", "Republican")) %>%
-  ggplot(
-    aes(
-      y = party,
-      x = `Avg. prob. toxic`,
-      color = party,
-      label = candidate
-    )
-  ) +
-  geom_jitter() +
-  ggrepel::geom_text_repel(max.overlaps = 100) +
-  scale_color_brewer(palette = "Set1", direction = -1) +
-  labs(y = "")
-ggarrange(temp0notext, temp1, ncol = 1)
-
-fb_house %>%
-  group_by(candidate, party) %>%
-  dplyr::summarise(
-    `Avg. prob. toxic` = mean(toxicity),
-    N = n()
-  ) %>%
-  dplyr::arrange(-`Avg. prob. toxic`) %>%
-  filter(str_detect(candidate, "GREEN"))
